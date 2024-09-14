04af941cd27f7e7b7559510ace6fe9beed511751
Carson Buttars <carsonbuttars13@gmail.com>
2024-09-13 23:11:52 -0600

    Add poisson models and various new variables: - rush_hour - hour_category - weekday - interactions between these and other variables

--- BikeShare.R
+++ BikeShare.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(tidymodels)
 library(vroom)
+library(poissonreg)
 
 # Read in the data
 train_df <- vroom("train.csv")
@@ -30,30 +31,35 @@ train_df[(15 >= train_df$hour) & (train_df$hour > 10), "hour_category"] <- 2
 train_df[(19 >= train_df$hour) & (train_df$hour > 15), "hour_category"] <- 3
 train_df["hour_category"] <- train_df$hour_category %>%
   as_factor()
-test_df["hour"] <- test_df$datetime %>%
-  hour()
 train_df["weekday"] <- train_df$datetime %>%
   weekdays() %>%
   as_factor()
-test_df["weekday"] <- test_df$datetime %>%
-  weekdays() %>%
-  as_factor()
+train_df["rush_hour"] <- train_df$hour %in% c(7, 8, 17, 18)
+test_df["hour"] <- test_df$datetime %>%
+  hour()
 test_df["hour_category"] <- 4
 test_df[(10 >= test_df$hour) & (test_df$hour > 5), "hour_category"] <- 1
 test_df[(15 >= test_df$hour) & (test_df$hour > 10), "hour_category"] <- 2
 test_df[(19 >= test_df$hour) & (test_df$hour > 15), "hour_category"] <- 3
 test_df["hour_category"] <- test_df$hour_category %>%
   as_factor()
+test_df["weekday"] <- test_df$datetime %>%
+  weekdays() %>%
+  as_factor()
+test_df["rush_hour"] <- test_df$hour %in% c(7, 8, 17, 18)
 
 #####################
 # linear regression #
 #####################
 
 # Variable selection
-tmp_df <- train_df[c(-1, -10, -11)]
-full_model <- lm(log(count) ~ ., data = tmp_df)
+tmp_df <- train_df[c(-1, -10, -11, -13)]
+full_model <- lm(log(count) ~ . +
+                   rush_hour * holiday +
+                   weekday * hour_category,
+                 data = tmp_df)
 base_model <- lm(log(count) ~ 1, data = tmp_df)
-selected <- stats::step(base_model,
+selected <- stats::step(full_model,
                         direction = "both",
                         scope = list(upper = full_model, lower = base_model))
 
@@ -73,7 +79,9 @@ registered_model <- linear_reg() %>%
 overall_model <- linear_reg() %>%
   set_engine("lm") %>%
   set_mode("regression") %>%
-  fit(formula = log(count) ~ ., data = train_df[c(-1, -6, -10, -11)])
+  fit(formula = log(count) ~ . +
+        rush_hour * holiday +
+        weekday * hour_category, data = train_df[c(-1, -6, -10, -11)])
 
 # Making predictions
 casual_predictions <- predict(casual_model, new_data = test_df)
@@ -89,4 +97,85 @@ overall_output <- tibble(datetime = as.character(format(test_df$datetime)),
                          count = overall_predictions)
 vroom_write(combination_output, "separate_linear_regressions.csv", delim = ",")
 vroom_write(overall_output, "linear_regression.csv", delim = ",")
+
+######################
+# Poisson regression #
+######################
+
+# Variable selection
+tmp_df <- train_df[c(-1, -11, -12, -13)]
+full_casual_model <- glm(casual ~ . +
+                           rush_hour * holiday +
+                           weekday * hour_category,
+                         data = tmp_df,
+                         family = poisson())
+base_casual_model <- glm(casual ~ 1,
+                         data = tmp_df,
+                         family = poisson())
+scope <- list(upper = full_casual_model, lower = base_casual_model)
+casual_poisson_selected <- stats::step(full_casual_model,
+                                       direction = "both",
+                                       scope = scope)
+tmp_df <- train_df[c(-1, -10, -12, -13)]
+full_registered_model <- glm(registered ~ . +
+                               rush_hour * holiday +
+                               weekday * hour_category,
+                             data = tmp_df,
+                             family = poisson())
+base_registered_model <- glm(registered ~ 1,
+                             data = tmp_df,
+                             family = poisson())
+scope <- list(upper = full_registered_model, lower = base_registered_model)
+registered_poisson_selected <- stats::step(full_registered_model,
+                                           direction = "both",
+                                           scope = scope)
+tmp_df <- train_df[c(-1, -10, -11, -13)]
+full_model <- glm(count ~ . +
+                    rush_hour * holiday +
+                    weekday * hour_category,
+                  data = tmp_df,
+                  family = poisson())
+base_model <- glm(count ~ 1,
+                  data = tmp_df,
+                  family = poisson())
+scope <- list(upper = full_model, lower = base_model)
+poisson_selected <- stats::step(full_model,
+                                direction = "both",
+                                scope = scope)
+
+# Make models
+casual_model <- poisson_reg() %>%
+  set_engine("glm") %>%
+  set_mode("regression") %>%
+  fit(formula = casual_poisson_selected$formula, data = train_df)
+registered_model <- poisson_reg() %>%
+  set_engine("glm") %>%
+  set_mode("regression") %>%
+  fit(formula = registered_poisson_selected$formula, data = train_df)
+overall_model <- poisson_reg() %>%
+  set_engine("glm") %>%
+  set_mode("regression") %>%
+  fit(formula = poisson_selected$formula, data = train_df)
+
+# Make predictions
+casual_predictions <- predict(casual_model, new_data = test_df)$.pred
+registered_predictions <- predict(registered_model, new_data = test_df)$.pred
+combination <- casual_predictions + registered_predictions
+overall_predictions <- predict(overall_model, new_data = test_df)$.pred
+no_select_poisson_predictions <- predict(full_model, new_data = test_df)
+
+# Organize and write output
+combination_output <- tibble(datetime = as.character(format(test_df$datetime)),
+                             count = combination)
+overall_output <- tibble(datetime = as.character(format(test_df$datetime)),
+                         count = overall_predictions)
+no_select_poisson_output <- tibble(datetime = test_df$datetime %>%
+                                     format() %>%
+                                     as.character(),
+                                   count = overall_predictions)
+vroom_write(combination_output, "separate_poisson_regression.csv", delim = ",")
+vroom_write(overall_output, "poisson_regression.csv", delim = ",")
+vroom_write(no_select_poisson_output,
+            "no_select_poisson_regression.csv",
+            delim = ",")
 
