5623aeee6f97ff0c0ed8e3e643a824e475efe012
Carson Buttars <carsonbuttars13@gmail.com>
2024-09-23 08:40:40 -0600

    Add automatic tuning for the penalized regression.

--- BikeShare.R
+++ BikeShare.R
@@ -31,39 +31,23 @@ penalized_recipe <- recipe %>%
 prepped_recipe <- prep(recipe)
 clean_data <- bake(prepped_recipe, new_data = train_df_dirty)
 
-# Models
+#####################
+# Linear regression #
+#####################
+
+# Create the model
 linear_model <- linear_reg() %>%
   set_engine("lm") %>%
   set_mode("regression")
-poisson_model <- poisson_reg() %>%
-  set_engine("glm") %>%
-  set_mode("regression")
-penalized_model <- linear_reg(penalty = 0.05, mixture = 0.5) %>%
-  set_engine("glmnet") %>%
-  set_mode("regression")
 
-# Create workflows
+# Create the workflow
 linear_workflow <- workflow() %>%
   add_model(linear_model) %>%
   add_recipe(recipe)
-poisson_workflow <- workflow() %>%
-  add_model(poisson_model) %>%
-  add_recipe(recipe)
-penalized_workflow <- workflow() %>%
-  add_model(penalized_model) %>%
-  add_recipe(penalized_recipe)
 
-# Fit models
+# Fit model and make predictions
 linear_fit <- fit(linear_workflow, data = log_train_df_dirty)
-poisson_fit <- fit(poisson_workflow, data = train_df_dirty)
-penalized_fit <- fit(penalized_workflow, data = log_train_df_dirty)
-
-# Make predictions
 linear_predictions <- predict(linear_fit, new_data = test_df_dirty)$.pred %>%
-  exp()
-poisson_predictions <- predict(poisson_fit, new_data = test_df_dirty)$.pred
-penalized_predictions <- predict(penalized_fit,
-                                 new_data = test_df_dirty)$.pred %>%
   exp()
 
 # Write output
@@ -71,15 +55,71 @@ linear_output <- tibble(datetime = test_df_dirty$datetime %>%
                           format() %>%
                           as.character(),
                         count = linear_predictions)
+vroom_write(linear_output, "linear_regression.csv", delim = ",")
+
+######################
+# Poisson regression #
+######################
+
+# Create the model
+poisson_model <- poisson_reg() %>%
+  set_engine("glm") %>%
+  set_mode("regression")
+
+# Create the workflow
+poisson_workflow <- workflow() %>%
+  add_model(poisson_model) %>%
+  add_recipe(recipe)
+
+# Fit model and make predictions
+poisson_fit <- fit(poisson_workflow, data = train_df_dirty)
+poisson_predictions <- predict(poisson_fit, new_data = test_df_dirty)$.pred
+
+# Write output
 poisson_output <- tibble(datetime = test_df_dirty$datetime %>%
                            format() %>%
                            as.character(),
                          count = poisson_predictions)
+vroom_write(poisson_output, "poisson_regression.csv", delim = ",")
+
+########################
+# Penalized regression #
+########################
+
+# Create the model
+penalized_model <- linear_reg(penalty = tune(), mixture = tune()) %>%
+  set_engine("glmnet") %>%
+  set_mode("regression")
+
+# Create the workflow
+penalized_workflow <- workflow() %>%
+  add_model(penalized_model) %>%
+  add_recipe(penalized_recipe)
+
+# Cross validate
+param_grid <- grid_regular(penalty(),
+                           mixture(),
+                           levels = 10)
+folds <- vfold_cv(log_train_df_dirty, v = 10, repeats = 1)
+cv_results <- penalized_workflow %>%
+  tune_grid(resamples = folds,
+            grid = param_grid,
+            metrics = metric_set(rmse))
+best_tune <- cv_results %>%
+  select_best(metric = "rmse")
+
+# Fit model and make predictions
+penalized_fit <- penalized_workflow %>%
+  finalize_workflow(best_tune) %>%
+  fit(data = log_train_df_dirty)
+penalized_predictions <- predict(penalized_fit,
+                                 new_data = test_df_dirty)$.pred %>%
+  exp()
+
+# Write output
 penalized_output <- tibble(datetime = test_df_dirty$datetime %>%
                              format() %>%
                              as.character(),
                            count = penalized_predictions)
-vroom_write(linear_output, "linear_regression.csv", delim = ",")
-vroom_write(poisson_output, "poisson_regression.csv", delim = ",")
 vroom_write(penalized_output, "penalized_regression.csv", delim = ",")
 
